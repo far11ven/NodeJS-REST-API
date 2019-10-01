aca305b43497e839dee6159a7b1473c8fc2a74e4
far11ven <kbhalaik@qualys.com>
2019-10-01 17:08:35 +0530

    added productImage add and Retreive capability

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-crypto = require("crypto");
+const crypto = require("crypto");
+const Grid = require("gridfs-stream");
 const multer = require("multer");
 // const storage = multer.diskStorage({
 //   destination: function(req, file, callback) {
@@ -14,6 +15,7 @@ const multer = require("multer");
 
 const GridFsStorage = require("multer-gridfs-storage");
 
+const getProductImageName = "Prod_" + Date.now();
 const connection = mongoose.connect(
   "mongodb+srv://" +
     process.env.MONGO_USER +
@@ -36,7 +38,7 @@ const storage = new GridFsStorage({
         if (err) {
           return reject(err);
         }
-        const filename = file.originalname;
+        const filename = getProductImageName;
         const fileInfo = {
           filename: filename,
           bucketName: "uploads"
@@ -103,12 +105,12 @@ router.get("/:productId", (req, res, next) => {
 
       if (doc) {
         res.status(200).json({
-          message: "GET requests for /products/{productId}",
+          message: "GET requests for /products/" + id,
           document: doc
         });
       } else {
         res.status(404).json({
-          message: "GET requests for /products/{productId}",
+          message: "GET requests for /products/" + id,
           error: "No Doc is present with passed ObjectId"
         });
       }
@@ -116,19 +118,96 @@ router.get("/:productId", (req, res, next) => {
     .catch(err => {
       console.log(err);
       res.status(500).json({
-        message: "GET requests for /products/{productId}",
+        message: "GET requests for /products/" + id,
         error: err
       });
     });
 });
 
+router.get("/:productId/image", (req, res, next) => {
+  try {
+    const id = req.params.productId;
+
+    Product.findOne({ _id: id })
+      .exec()
+      .then(doc => {
+        console.log("doc >>> ", doc);
+
+        const imageFileId = doc.productImage.file_id;
+
+        if (doc) {
+          let gfs;
+          const conn = mongoose.createConnection(process.env.MONGO_URI);
+          conn.once("open", () => {
+            gfs = Grid(conn.db, mongoose.mongo);
+
+            gfs.collection("uploads");
+            console.log("imageFileId >>>" + imageFileId);
+
+            gfs.files.findOne(
+              { _id: mongoose.Types.ObjectId(imageFileId) },
+              (err, file) => {
+                // Check if file
+                if (!file || file.length === 0) {
+                  return res.status(404).json({
+                    err: "No file exists"
+                  });
+                }
+
+                // Check if its an image
+                if (
+                  file.contentType === "image/jpeg" ||
+                  file.contentType === "image/png"
+                ) {
+                  // Read output to browser
+                  const readstream = gfs.createReadStream(file.filename);
+                  readstream.pipe(res);
+                } else {
+                  res.status(404).json({
+                    err: "Not an image"
+                  });
+                }
+              }
+            );
+          });
+        } else {
+          res.status(404).json({
+            error: "No product present with ObjectId : " + id
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          message:
+            "Not able to get 'productImage.file_id' from product with" + id,
+          error: err
+        });
+      });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "GET requests for /products",
+      error: err
+    });
+  }
+});
+
 router.post("/", upload.single("productImage"), (req, res, next) => {
   console.log(" file ==> ", req.file);
+
+  const productId = new mongoose.Types.ObjectId();
+
+  const productImageObj = {
+    file_id: mongoose.Types.ObjectId(req.file.id),
+    url: "/api/products/" + productId + "/image"
+  };
+
   const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
+    _id: productId,
     name: req.body.name,
     price: req.body.price,
-    productImage: "asd"
+    productImage: productImageObj
   });
 
   product
@@ -141,6 +220,10 @@ router.post("/", upload.single("productImage"), (req, res, next) => {
           name: result.name,
           price: result.price,
           _id: result.id,
+          productImage: {
+            file_id: result.productImage.file_id,
+            url: result.productImage.url
+          },
           request: {
             type: "GET",
             url: "/api/products/" + result.id
@@ -168,14 +251,14 @@ router.patch("/:productId", (req, res, next) => {
     .then(result => {
       console.log(result);
       res.status(200).json({
-        message: "UPDATE requests for /products/{productId}",
+        message: "UPDATE requests for /products/" + id,
         result: result
       });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json({
-        message: "UPDATE requests for /products/{productId}",
+        message: "UPDATE requests for /products/" + id,
         error: err
       });
     });
@@ -189,14 +272,14 @@ router.delete("/:productId", (req, res, next) => {
     .then(result => {
       console.log(result);
       res.status(200).json({
-        message: "DELETE requests for /products/{productId}",
+        message: "DELETE requests for /products/" + id,
         result: result
       });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json({
-        message: "DELETE requests for /products/{productId}",
+        message: "DELETE requests for /products/" + id,
         error: err
       });
     });
--- models/product.js
+++ models/product.js
@@ -4,7 +4,10 @@ const productSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
   price: { type: Number, required: true },
-  productImage: { type: String, required: true }
+  productImage: {
+    file_id: mongoose.Schema.Types.ObjectId,
+    url: { type: String, required: true }
+  }
 });
 
 module.exports = mongoose.model("Product", productSchema);
